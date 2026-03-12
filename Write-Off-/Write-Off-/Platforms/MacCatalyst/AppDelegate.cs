34df3cf33643b2810654680e53ce8474cc3a1a75
Sietse Drent (student) <s1113887@student.windesheim.nl>
2026-03-12 16:30:44 +0100

    Initial commit

---
+++ Write-Off-/Write-Off-/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Write_Off_
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
